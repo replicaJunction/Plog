498dd7fcd182bdde5879d0ca73384b5605cfdf7a
Joshua T <joshua@sonofone.net>
2016-02-16 14:15:58 -0600

    Created function Set-LogMode

---
+++ Functions/Set-LogMode.Tests.ps1
@@ -0,0 +1,57 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+InModuleScope 'Plog' {    
+    Describe 'Set-LogMode' {
+        
+        Mock Get-ModulePrivateData {
+            Write-Output @{}
+        }
+        
+        Context 'LogToFile' {
+            $filePath = "TestDrive:\dir\temp.log"
+            
+            Mock Set-ModulePrivateData {
+                # Export what would be set to the PrivateData FilePath variable
+                $PrivateData.FilePath
+            }
+        
+            It 'Creates the log file if it does not exist' {
+                { Set-LogMode -FilePath $filePath } | Should Not Throw
+                $filePath | Should Exist
+            }
+            
+            It 'Updates the module PrivateData with file path and settings' {
+                $output = Set-LogMode -FilePath $filePath
+                $output | Should BeExactly $filePath
+            }
+        }
+        
+        Context 'LogToEventLog' {
+            Mock Write-EventLog -Verifiable {}
+            
+            Mock Set-ModulePrivateData {
+                @{
+                    Source = $PrivateData.Source
+                    LogName = $PrivateData.LogName
+                }
+            }
+            
+            It 'Tries to create an example event log entry if -NoTest is not specified' {
+                { Set-LogMode -EventLog } | Should Not Throw
+                Assert-MockCalled -CommandName Write-EventLog -Scope It -Times 1 -Exactly
+                
+                # Should not be called a second time
+                { Set-LogMode -EventLog -NoTest } | Should Not Throw
+                Assert-MockCalled -CommandName Write-EventLog -Scope It -Times 1 -Exactly
+            }
+            
+            It 'Updates the module PrivateData with event source and log name' {
+                $output = Set-LogMode -EventLog -LogName 'LogName1' -Source 'Source1' -NoTest
+                $output.LogName | Should Be 'LogName1'
+                $output.Source | Should Be 'Source1'
+            }
+        }
+    }
+}
---
+++ Functions/Set-LogMode.ps1
@@ -0,0 +1,92 @@
+﻿function Set-LogMode {
+    [CmdletBinding(DefaultParameterSetName = 'LogToFile')]
+    param(
+        # Path to the log file to use
+        [Parameter(ParameterSetName = 'LogToFile',
+                   Mandatory = $true)]
+        [String] $FilePath,
+        
+        # Indicates that logging should be done to the Windows event log.
+        [Parameter(ParameterSetName = 'LogToEventLog',
+                   Mandatory = $true)]
+        [Switch] $EventLog,
+        
+        # Name of the log to use. If not using the default, try Application.
+        [Parameter(ParameterSetName = 'LogToEventLog',
+                   Mandatory = $false)]
+        [String] $LogName = 'Windows PowerShell',
+        
+        # Event source to use. Note that this event source must already exist!
+        [Parameter(ParameterSetName = 'LogToEventLog',
+                   Mandatory = $false)]
+        [String] $Source = 'PowerShell',
+        
+        # Do not create a test event log entry. This is potentially dangerous
+        # if you are using non-default log names or sources.
+        [Parameter(ParameterSetName = 'LogToEventLog',
+                   Mandatory = $false)]
+        [Switch] $NoTest
+    )
+    
+    begin {
+        
+        $p = Get-ModulePrivateData
+        
+    }
+    
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'LogToEventLog' {
+                if (-not $NoTest) {
+                    Write-Verbose "Creating a test event log entry to log $LogName with source $Source"
+                    try {
+                        Write-EventLog -LogName $LogName -Source $Source -EventID 12345 -EntryType Information -Message 'Plog is testing logging to the Windows Event Log. If this event is logged successfully, there are no problems.'
+                    }
+                    catch [System.Exception] {
+                        $err = $_
+                        throw "Plog was unable to log to the Windows Event Log: $err"
+                    }
+                }
+                else {
+                    Write-Verbose "-NoTest was specified. No test log entry will be created."                    
+                }
+                
+                $p.Mode = 'EventLog'
+                $p.Source  = $Source
+                $p.LogName = $LogName
+            }
+            
+            'LogToFile' {
+                if (-not (Test-Path -Path $FilePath)) {
+                    Write-Verbose "Log file $FilePath does not exist. Attempting to create the file..."
+                    try {
+                        $directory = Split-Path -Path $FilePath -Parent
+                        if (-not (Test-Path -Path $directory)) {
+                            Write-Verbose "Creating parent directory $directory"
+                            $null = New-Item -Path $directory -ItemType Directory -Force
+                        }
+                        
+                        $null = New-Item -Path $FilePath -ItemType File -Force
+                    }
+                    catch [System.Exception] {
+                        $err = $_
+                        throw "Unable to access path ${FilePath}: $err"
+                    }
+                }
+                else {
+                    Write-Verbose "Log file $FilePath already exists."
+                }
+                
+                $p.Mode = 'File'
+                $p.FilePath = $FilePath
+            }
+        }
+    }
+    
+    end {
+        
+        # Update PrivateData again
+        Set-ModulePrivateData -PrivateData $p
+        
+    }
+}
