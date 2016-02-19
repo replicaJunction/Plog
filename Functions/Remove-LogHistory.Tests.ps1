315fc54b7e4c6df06f818aa8e22f2787247e535b
Joshua T <joshua@sonofone.net>
2016-02-19 17:09:03 -0600

    Created function Remove-LogHistory

---
+++ Functions/Remove-LogHistory.Tests.ps1
@@ -0,0 +1,126 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+InModuleScope 'Plog' {
+    Describe 'Remove-LogHistory' {
+        
+        $privateData = @{
+            Mode    = 'File'
+            Path    = 'TestDrive:\dir'
+            History = @{
+                Mode       = 'StructuredFolder'
+                Days       = 2
+                Weeks      = 2
+                Months     = 1
+                DayOfWeek  = 2 # Monday
+                DayOfMonth = 1 # 1st of every month
+            } 
+        }
+    
+        Mock Get-ModulePrivateData { $privateData }
+        Mock Write-Debug { Write-Host $Message -ForegroundColor Cyan }
+        
+        $dateFormat = 'yyyyMMdd-HHmm'
+        
+        # Name of this script file, without the .ps1 extension.
+        # MyInvocation.MyCommand.Path and MyInvocation.ScriptName do weird
+        # things inside Pester.
+        $scriptName = 'Remove-LogHistory.Tests'
+        
+        # Init some helper variables
+        $dateToday = Get-Date
+        $dateYesterday = $dateToday.AddDays(-1)
+        $dateOneWeekAgo = $dateToday.AddDays(-7)
+        $dateFifteenDaysAgo = $dateToday.AddDays(-15)
+        $dateFirstOfMonth = New-Object -Type 'System.DateTime' -ArgumentList @(
+            $dateToday.Year,
+            $dateToday.Month,
+            1
+        )
+        
+        $logFileName = Get-LogFileName
+        Write-Host "Log file name: $logFileName" -ForegroundColor Yellow
+        $logDirectory = Split-Path -Path $logFileName -Parent
+        Write-Host "Log file directory: $logDirectory" -ForegroundColor Yellow
+        
+        # Make a "backup" reference to the real Get-Date so we can do some
+        # crazy mocks with it
+        $getDateCmdlet = Get-Command -Name Get-Date -CommandType Cmdlet
+        
+        #####
+        
+        # Helper function to create a log file from the provided date
+        function New-LogFileFromDate([DateTime] $targetDate) {
+            Mock Get-Date -ParameterFilter {$Date -eq $null} {
+                # Write-Host "    Mocked Get-Date: date=[$($Date)], format=[$($Format)]"
+                # Invoke the original Get-Date to format our target date
+                & $getDateCmdlet -Date $targetDate -Format $Format
+            }
+            
+            # Create a log entry
+            Write-Log "Log entry from date $targetDate"
+            
+            $filename = Get-LogFileName
+            # Write-Host "Created log file from $targetDate at path $filename"
+            
+            # Modify file attributes
+            $file = Get-Item -Path $filename
+            $file.CreationTime = $targetDate
+            $file.LastWriteTime = $targetDate
+            
+            # Return a reference to the file
+            Write-Output $file
+        }
+        
+        #####
+        
+        Context 'Sanity checking' {
+            $testFile = New-LogFileFromDate $dateOneWeekAgo
+                
+            It 'New-LogFileFromDate helper function works correctly' {
+                $testFile | Should Exist
+                $testFile.CreationTime | Should Be $dateOneWeekAgo
+                $testFile.LastWriteTime | Should Be $dateOneWeekAgo
+            }
+        }
+        
+        # Create some example log files
+        $fileToday          = New-LogFileFromDate $dateToday
+        $fileYesterday      = New-LogFileFromDate $dateYesterday
+        $fileOneWeekAgo     = New-LogFileFromDate $dateOneWeekAgo
+        $fileFifteenDaysAgo = New-LogFileFromDate $dateFifteenDaysAgo
+        $fileFirstOfMonth   = New-LogFileFromDate $dateFirstOfMonth
+        
+        # Un-mock the Get-Date function
+        Mock Get-Date -ParameterFilter {$Date -eq $null} { & $getDateCmdlet -Format $Format @PSBoundParameters }
+        
+        # Run the function
+        Remove-LogHistory
+        
+        It 'Creates daily, weekly, and monthly log folders' {
+            "$logDirectory\daily" | Should Exist
+            "$logDirectory\weekly" | Should Exist
+            "$logDirectory\monthly" | Should Exist
+        }
+        
+        It 'Deletes files in the original directory older than today' {
+            $fileToday | Should Exist
+            $fileYesterday | Should Not Exist
+            $fileFifteenDaysAgo | Should Not Exist
+            $fileFirstOfMonth | Should Not Exist
+        }
+        
+        It 'Copies files newer than X days into the Daily folder' {
+            $fileTodayDaily = $fileYesterdayDaily = Join-Path -Path "$logDirectory\daily" -ChildPath (Split-Path -Path $fileToday.FullName -Leaf)
+            $fileTodayDaily | Should Exist
+            $fileYesterdayDaily = Join-Path -Path "$logDirectory\daily" -ChildPath (Split-Path -Path $fileYesterday.FullName -Leaf)
+            $fileYesterdayDaily | Should Exist
+        }
+        
+        It 'Copies files from the X day of the month into the Monthly folder' {
+            $fileFirstOfMonthMonthly = Join-Path -Path "$logDirectory\monthly" -ChildPath (Split-Path -Path $fileFirstOfMonth -Leaf)
+            $fileFirstOfMonthMonthly | Should Exist
+        }
+    }
+}
---
+++ Functions/Remove-LogHistory.ps1
@@ -0,0 +1,150 @@
+﻿function Remove-LogHistory {
+    [CmdletBinding()]
+    param()
+    
+    begin {
+        # Constants
+        if (-not $script:regexDate) {
+            # Regex to get the date out of a log file name
+            $script:regexDate = '^.*_(?<date>\d{8}-\d{4})\.log$'
+        }
+        
+        if (-not $script:fileDateFormat) {
+            $script:fileDateFormat = 'yyyyMMdd-HHmm'
+        }
+        
+        $p = Get-ModulePrivateData
+        
+        # Daily files must be newer than x days old
+        $dateDailyCutoff = [DateTime]::Today.AddDays(-$p.History.Days)
+        
+        # Weekly files must be newer than x weeks old AND must be from the
+        # configured day of the week. 
+        $dateWeeklyCutoff = [DateTime]::Today.AddDays(-$p.History.Weeks * 7)
+        
+        # Monthly files must be newer than x months old AND must be from the
+        # configured day of the month.
+        # This is an average for days of the month. There may be a few fringe
+        # cases that this will miss.
+        $dateMonthlyCutoff = [DateTime]::Today.AddDays(-$p.History.Months * 30)
+        
+        $currentLogFile = Get-LogFileName -ScriptName (Split-Path -Path $MyInvocation.ScriptName -Leaf)
+        $logDir = Split-Path -Path $currentLogFile -Parent
+        $logBaseName = ((Split-Path -Path $currentLogFile -Leaf) -split '_')[0] # Remove the date stamp
+        
+        if ($p.History.Mode -eq 'StructuredFolder') {
+            $dirDaily = Join-Path -Path $logDir -ChildPath 'daily'
+            $dirWeekly = Join-Path -Path $logDir -ChildPath 'weekly'
+            $dirMonthly = Join-Path -Path $logDir -ChildPath 'monthly'
+            
+            try {
+                $dirDaily, $dirWeekly, $dirMonthly | % {
+                    if (-not (Test-Path -Path $_)) {
+                        [void] (New-Item -Path $_ -ItemType Directory -Force)
+                    }
+                }
+            }
+            catch [System.IOException] {
+                $err = $_
+                Write-Debug "Plog encountered an exception creating log archives"
+                throw $err
+            }
+        }
+    }
+    
+    process {
+        $rootLogFiles = Get-ChildItem -Path $logDir -Filter "$logBaseName*.log"
+        foreach ($file in $rootLogFiles) {
+            Write-Debug "Remove-LogHistory: processing file $($file.FullName)"
+            
+            # Get the date out of the filename using a named group in the regex
+            # and PowerShell's automatic $Matches variable
+            if ($file.Name -match $script:regexDate) {
+                # Parse the text date as a DateTime
+                $thisDate = [DateTime]::ParseExact($Matches.date, $script:fileDateFormat, [System.Globalization.CultureInfo]::CurrentCulture)
+                
+                if ($thisDate -ge $dateDailyCutoff) {
+                    Write-Debug "Remove-LogHistory: file is less than $($p.History.Days) days old; copying to Daily directory"
+                    Copy-Item -Path $file.FullName -Destination $dirDaily                    
+                }
+                
+                if ($thisDate -ge $dateWeeklyCutoff -and [int] $thisDate.DayOfWeek -eq $p.History.DayOfWeek) {
+                    Write-Debug "Remove-LogHistory: file is from $($thisDate.DayOfWeek); copying to Weekly directory"
+                    Copy-Item -Path $file.FullName -Destination $dirWeekly
+                }
+                
+                if ($thisDate -ge $dateMonthlyCutoff -and [int] $thisDate.Day -eq $p.History.DayOfMonth) {
+                    Write-Debug "Remove-LogHistory: file is from day $($thisDate.Day) of the month; copying to Monthly directory"
+                    Copy-Item -Path $file.FullName -Destination $dirMonthly
+                }
+                
+                if ($thisDate -ge [DateTime]::Today) {
+                    Write-Debug "Remove-LogHistory: file is from today. No action will be taken."
+                }
+                else {
+                    Write-Debug "Remove-LogHistory: removing original file"
+                    Remove-Item -Path $file.FullName -Force
+                }
+            }
+            else {
+                Write-Debug "Remove-LogHistory: unknown file [$file] will be removed"
+                Remove-Item -Path $file.FullName -Force
+            }
+        }
+        
+        # Clean daily files
+        
+        $dailyLogFiles = Get-ChildItem -Path $dirDaily
+        foreach ($file in $dailyLogFiles) {
+            if ($file.Name -match $script:regexDate) {
+                # Parse the text date as a DateTime
+                $thisDate = [DateTime]::ParseExact($Matches.date, $script:fileDateFormat, [System.Globalization.CultureInfo]::CurrentCulture)
+                
+                if ($thisDate -lt $dateDailyCutoff) {
+                    Write-Debug "Remove-LogHistory: daily file [$file] is past the cutoff date and will be removed"
+                    Remove-Item -Path $file.FullName -Force
+                }             
+            }
+            else {
+                Write-Debug "Remove-LogHistory: unknown file [$file] will be removed"
+                Remove-Item -Path $file.FullName -Force
+            }
+        }
+        
+        # Clean weekly files
+        $weeklyLogFiles = Get-ChildItem -Path $dirWeekly
+        foreach ($file in $weeklyLogFiles) {
+            if ($file.Name -match $script:regexDate) {
+                # Parse the text date as a DateTime
+                $thisDate = [DateTime]::ParseExact($Matches.date, $script:fileDateFormat, [System.Globalization.CultureInfo]::CurrentCulture)
+                
+                if ($thisDate -lt $dateWeeklyCutoff) {
+                    Write-Debug "Remove-LogHistory: weekly file [$file] is past the cutoff date and will be removed"
+                    Remove-Item -Path $file.FullName -Force
+                }             
+            }
+            else {
+                Write-Debug "Remove-LogHistory: unknown file [$file] will be removed"
+                Remove-Item -Path $file.FullName -Force
+            }
+        }
+        
+        # Clean monthly files
+        $monthlyLogFiles = Get-ChildItem -Path $dirMonthly
+        foreach ($file in $monthlyLogFiles) {
+            if ($file.Name -match $script:regexDate) {
+                # Parse the text date as a DateTime
+                $thisDate = [DateTime]::ParseExact($Matches.date, $script:fileDateFormat, [System.Globalization.CultureInfo]::CurrentCulture)
+                
+                if ($thisDate -lt $dateMonthlyCutoff) {
+                    Write-Debug "Remove-LogHistory: weekly file [$file] is past the cutoff date and will be removed"
+                    Remove-Item -Path $file.FullName -Force
+                }             
+            }
+            else {
+                Write-Debug "Remove-LogHistory: unknown file [$file] will be removed"
+                Remove-Item -Path $file.FullName -Force
+            }
+        }
+    }
+}
