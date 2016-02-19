9931bdb79585f2acf0bd258f34d69b0b09178625
Joshua T <joshua@sonofone.net>
2016-02-19 09:17:52 -0600

    Created function Set-LogHistoryMode
    This function will allow users to define history settings for how much log data to preserve

---
+++ Functions/Set-LogHistoryMode.Tests.ps1
@@ -0,0 +1,28 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+InModuleScope 'Plog' {
+    Describe 'Set-LogHistoryMode' {
+        $privateData = @{
+            Mode    = 'File'
+            Path    = 'TestDrive:\dir'
+            History = @{} 
+        }
+    
+        Mock Get-ModulePrivateData { $privateData }
+        Mock Set-ModulePrivateData { $privateData }
+        
+        Mock Write-Debug { Write-Host $Message -ForegroundColor Cyan }
+        
+        It 'Updates module PrivateData with the provided history mode' {
+            $output = Set-LogHistoryMode -Mode 'Simple'
+            $output.History.Mode | Should Be 'Simple'
+        }
+        
+        It 'Correctly handles positional parameters' {
+            $output = Set-LogHistoryMode 'StructuredFolder'
+            $output.History.Mode | Should Be 'StructuredFolder'
+        }
+    }
+}
---
+++ Functions/Set-LogHistoryMode.ps1
@@ -0,0 +1,28 @@
+﻿function Set-LogHistoryMode {
+    [CmdletBinding()]
+    param(
+        # Log history mode.
+        # Simple - create timestamped log files in the configured path
+        # StructuredFolder - create subfolders for log history by day and week
+        [Parameter(Mandatory = $false,
+                   Position = 0)]
+        [ValidateSet('Simple','StructuredFolder')]
+        [String] $Mode = 'Simple'
+    )
+        
+    begin {
+        $p = Get-ModulePrivateData
+    }
+    
+    process {
+        if (-not $p.History) {
+            $p.History = @{}
+        }
+        
+        $p.History.Mode = $Mode
+    }
+    
+    end {
+        Set-ModulePrivateData -PrivateData $p
+    }
+}
