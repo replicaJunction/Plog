f90b45b71c41128c070f3bee5adf60fa0069dcc6
Joshua T <joshua@sonofone.net>
2016-02-23 11:32:55 -0600

    Fixed new log files appearing every minute.
    This also improves performance by using a cached log filename.

--- Functions/Internal/Get-LogFileName.Tests.ps1
+++ Functions/Internal/Get-LogFileName.Tests.ps1
@@ -26,32 +26,44 @@ InModuleScope 'Plog' {
         # We're going to mock Get-Date to ensure that there's no millisecond difference
         # between the test and the function.
         $dateConstant = Get-Date -Format $dateFormat 
-        Mock Get-Date -ParameterFilter {$Format -eq $dateFormat} {
-            $dateConstant
+        Mock Get-Date -ParameterFilter {$Format -eq $dateFormat} { $dateConstant }
+        
+        $output = Get-LogFileName
+        $expectedFilename = 'TestDrive:\dir\{0}_{1}.log' -f $scriptName, $dateConstant
+        
+        It 'Gets a log file name with the current script name a date and timestamp' {    
+            $output | Should Be $expectedFilename
         }
         
-        It 'Gets a log file name with the current script name a date and timestamp' {
-            $expectedFilename = 'TestDrive:\dir\{0}_{1}.log' -f $scriptName, $dateConstant
-            Get-LogFileName | Should Be $expectedFilename
+        It 'Creates the log directory if it does not exist' {
+            'TestDrive:\dir' | Should Exist
         }
         
-        It 'Returns a filename in a subfolder if History.Mode is set to StructuredFolder' {
+        It 'Uses a cached log file for performance' {
+            # Change a setting that would produce a different filename
             $privateData.History = @{
                 Mode = 'StructuredFolder'
             }
             
+            Get-LogFileName | Should Be $expectedFilename 
+        }
+        
+        $oldOutput = $output
+        $output = Get-LogFileName -Force
+        $expectedFilename = 'TestDrive:\dir\{0}\{0}_{1}.log' -f $scriptName, $dateConstant
+        
+        It 'Supports the -Force switch to generate a new filename' {
+            $output | Should Not Be $oldOutput
+        }
+        
+        It 'Returns a filename in a subfolder if History.Mode is set to StructuredFolder' {
             $expectedFilename = 'TestDrive:\dir\{0}\{0}_{1}.log' -f $scriptName, $dateConstant
-            Get-LogFileName | Should Be $expectedFilename
+            $output | Should Be $expectedFilename
         }
         
         It 'Accepts a -ScriptName parameter to provide an alternate script name' {
             $expectedFilename = 'TestDrive:\dir\{0}\{0}_{1}.log' -f 'MyScript001', $dateConstant
-            Get-LogFileName -ScriptName 'MyScript001' | Should Be $expectedFilename
-        }
-        
-        It 'Creates the log directory if it does not exist' {
-            $expectedDirectory = 'TestDrive:\dir\{0}' -f $scriptName
-            $expectedDirectory | Should Exist
+            Get-LogFileName -ScriptName 'MyScript001' -Force | Should Be $expectedFilename
         }
     }
 }
--- Functions/Internal/Get-LogFileName.ps1
+++ Functions/Internal/Get-LogFileName.ps1
@@ -4,7 +4,10 @@
         # Name of the script file calling Write-Log
         [Parameter(Mandatory = $false,
                    Position = 0)]
-        [String] $ScriptName
+        [String] $ScriptName,
+        
+        # Do not use the cached log filename 
+        [Switch] $Force
     )
     
     begin {
@@ -17,45 +20,53 @@
     }
     
     process {
-        if ($p.Mode -eq 'File') {
-            if (-not $ScriptName) {
-                # Get the filename of the main script file
-                $ScriptName = $MyInvocation.ScriptName | Split-Path -Leaf
-            }
-            Write-Debug "Get-LogFileName: ScriptName is [$ScriptName]"
-            if ($ScriptName -like '*.ps1') {
-                # Remove the .ps1 from the script name
-                $ScriptName = $ScriptName.substring(0, $ScriptName.LastIndexOf('.'))
-            }
-            Write-Debug "Get-LogFileName: Adjusted ScriptName is [$ScriptName]"
-
-            switch ($p.History.Mode) {
-                'StructuredFolder' {
-                    # Place the log file in a folder named after the script file
-                    $logFileName = '{0}\{0}_{1}.log' -f $ScriptName, (Get-Date -Format $script:fileDateFormat)
-                }
-                'Simple' {
-                    # Also includes 'Simple'. Place the log file in the path specified.
-                    $logFileName = '{0}_{1}.log' -f $ScriptName, (Get-Date -Format $script:fileDateFormat)
-                }
-                default {
-                    # This should never be hit
-                    throw "Unsupported History mode [$($p.History.Mode)]"
-                }
-            }
-            $result = Join-Path -Path $p.Path -ChildPath $logFileName  
-            Write-Debug "Get-LogFileName: returning value [$result]"
-            
-            $logDir = Split-Path -Path $result -Parent
-            if (-not (Test-Path -Path $logDir)) {
-                Write-Debug "Plog: creating log directory $logDir"
-                [void] (New-Item -Path $logDir -ItemType 'Directory' -Force)
-            }
-            
-            Write-Output $result
+        if ($script:logFileName -and -not $Force) {
+            Write-Debug "Get-LogFileName: Using cached log file [$($script:logFileName)]"
         }
         else {
-            Write-Debug "Not in File mode, so there is no filename for logging"
+            Write-Debug "Get-LogFileName: Building new log file name"
+            if ($p.Mode -eq 'File') {
+                if (-not $ScriptName) {
+                    # Get the filename of the main script file
+                    $ScriptName = $MyInvocation.ScriptName | Split-Path -Leaf
+                }
+                Write-Debug "Get-LogFileName: ScriptName is [$ScriptName]"
+                if ($ScriptName -like '*.ps1') {
+                    # Remove the .ps1 from the script name
+                    $ScriptName = $ScriptName.substring(0, $ScriptName.LastIndexOf('.'))
+                }
+                Write-Debug "Get-LogFileName: Adjusted ScriptName is [$ScriptName]"
+
+                switch ($p.History.Mode) {
+                    'StructuredFolder' {
+                        # Place the log file in a folder named after the script file
+                        $logFileName = '{0}\{0}_{1}.log' -f $ScriptName, (Get-Date -Format $script:fileDateFormat)
+                    }
+                    'Simple' {
+                        # Also includes 'Simple'. Place the log file in the path specified.
+                        $logFileName = '{0}_{1}.log' -f $ScriptName, (Get-Date -Format $script:fileDateFormat)
+                    }
+                    default {
+                        # This should never be hit
+                        throw "Unsupported History mode [$($p.History.Mode)]"
+                    }
+                }
+                $result = Join-Path -Path $p.Path -ChildPath $logFileName  
+                Write-Debug "Get-LogFileName: returning value [$result]"
+                
+                $logDir = Split-Path -Path $result -Parent
+                if (-not (Test-Path -Path $logDir)) {
+                    Write-Debug "Plog: creating log directory $logDir"
+                    [void] (New-Item -Path $logDir -ItemType 'Directory' -Force)
+                }
+                
+                $script:logFileName = $result
+            }
+            else {
+                Write-Debug "Not in File mode, so there is no filename for logging"
+            }
         }
+        
+        Write-Output $script:logFileName
     }
 }
